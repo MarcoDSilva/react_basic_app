373a75b22486b5d4e1d38fa262f19962d89f8c72
MarcoDSilva <38049196+MarcoDSilva@users.noreply.github.com>
2020-06-24 21:25:16 +0100

    testing handlers and state changes

--- src/index.js
+++ src/index.js
@@ -1,67 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
-const Header = (props) => {
-  return (
-    <>
-      <h1>{props.course.name}</h1>
-    </>
-  );
-};
-
-const Part = (props) => (
-  <>
-    <p>
-      {props.part.name}: {props.part.exercises}
-    </p>
-  </>
-);
-
-const Content = (props) => {
-  return (
-    <div>
-      <Part part={props.part[0]} />
-      <Part part={props.part[1]} />
-      <Part part={props.part[2]} />
-    </div>
-  );
-};
-
-const Total = (props) => {
-  const arr = props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises ;
-  return (
-    <>
-      <p>
-        Number of exercises: {arr}
-      </p>
-    </>
-  );
-};
-
 const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-      },
-      {
-        name: "Using prop to pass data",
-        exercises: 7,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-      },
-    ],
-  } 
-  
+  //func adds state to the component and renders it with value of 0
+  //counter is initialized with the value of state (0)
+  //set counter is assigned a func that will be used to modify the state
+  const [counter, setCounter] = useState(0);
+
+  const increment = () => {
+    setCounter(counter + 1);
+  };
+
+  const decrement = () => {
+    setCounter(counter - 1);
+  }
+
+  const reset = () => {
+    setCounter(0);
+  };
+
   return (
     <div>
-      <Header course={course} />
-      <Content part={course.parts} />
-      <Total parts={course.parts} />
+      <div> {counter} </div>
+      <button onClick={increment}> Plus </button>
+      <button onClick={decrement}>Minus</button>
+      <button onClick={reset}>Reset</button>
     </div>
   );
 };
