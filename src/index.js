5be5ddc6afe5eb6f91b474a363cfbaf7c6ebf8c9
MarcoDSilva <38049196+MarcoDSilva@users.noreply.github.com>
2020-06-24 22:25:25 +0100

    various states and saving previous state used

--- src/index.js
+++ src/index.js
@@ -1,38 +1,34 @@
-import React, { useState } from "react";
-import ReactDOM from "react-dom";
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
 
-const Display = ({ counter }) => <div>{counter}</div>;
+const App = (props) => {
+  //states for left, right and all clicks used
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
 
-const Button = ({ handleClick, text }) => {
-  return <button onClick={handleClick}>{text}</button>;
-};
+  //handlers for left/right click and update
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
 
-const App = () => {
-  //func adds state to the component and renders it with value of 0
-  //counter is initialized with the value of state (0)
-  //set counter is assigned a func that will be used to modify the state
-  const [counter, setCounter] = useState(0);
-
-  const increment = () => {
-    setCounter(counter + 1);
-  };
-
-  const decrement = () => {
-    setCounter(counter - 1);
-  };
-
-  const reset = () => {
-    setCounter(0);
-  };
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
 
   return (
     <div>
-      <Display counter={counter} />
-      <Button onClick={increment} text={"Plus"} />
-      <Button onClick={decrement} text={"Minus"} />
-      <Button onClick={reset} text={"Reset"} />
+      <div>
+        {left}
+        <button onClick={handleLeftClick}> left </button>
+        <button onClick={handleRightClick}> right </button>
+        {right}
+        <p>{allClicks.join(' ')}</p>
+      </div>
     </div>
-  );
-};
+  )
+}
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(<App />, document.querySelector('#root'))
