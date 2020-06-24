6808c68ca5512beceeda4f0d01f0ec0d0139db70
MarcoDSilva <38049196+MarcoDSilva@users.noreply.github.com>
2020-06-24 22:45:46 +0100

    component refactor

--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
 import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 
+//history render component to show the last plays state
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the butttons
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
+//btn component
+const Button = ({onClick, text}) => (
+  <button onClick={onClick}> {text} </button>
+)
+
 const App = (props) => {
   //states for left, right and all clicks used
   const [left, setLeft] = useState(0)
@@ -22,10 +44,10 @@ const App = (props) => {
     <div>
       <div>
         {left}
-        <button onClick={handleLeftClick}> left </button>
-        <button onClick={handleRightClick}> right </button>
+        <Button onClick={handleLeftClick} text="left" />
+        <Button onClick={handleRightClick} text="right" />
         {right}
-        <p>{allClicks.join(' ')}</p>
+        <History allClicks={allClicks}/>
       </div>
     </div>
   )
