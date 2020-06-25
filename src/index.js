6df8e43e8140b0df1407f0bd7ff7384feb896fbe
MarcoDSilva <38049196+MarcoDSilva@users.noreply.github.com>
2020-06-25 22:25:33 +0100

    1.12 exercise anedoctes

--- src/index.js
+++ src/index.js
@@ -1,61 +1,35 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
-const Button = ({ clickHandle, text }) => {
-  return <button onClick={clickHandle}>{text}</button>;
+const anecdotes = [
+  "If it hurts, do it more often",
+  "Adding manpower to a late software project makes it later!",
+  "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+  "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+  "Premature optimization is the root of all evil.",
+  "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+];
+
+const rng = () => Math.floor(Math.random() * anecdotes.length);
+
+const Button = ({ handleBtn, text }) => {
+  return <button onClick={() => handleBtn()}>{text}</button>;
 };
 
-const Statistics = ({ text, stat }) => {
-  return (
-    <tr>
-      <td>{text}:</td>
-      <td>{stat}</td>
-    </tr>
-  );
-};
+const App = (props) => {
+  const [selected, setSelected] = useState(0);
 
-const Stats = ({ good, neutral, bad }) => {
-  const all = good + neutral + bad;
-  const positive = ((good / all) * 100).toFixed(2) + '%'
-  const average = (good - bad) / all
-  
-  //conditional rendering
-  if (all === 0) {
-    return <p>No feedback given</p>;
+  const newQuote = () => {
+    let index = rng()
+    setSelected(index)
   }
 
   return (
     <div>
-      <h3>Statistics</h3>
-      <table>
-        <tbody>
-          <Statistics text="good" stat={good} />
-          <Statistics text="neutral" stat={neutral} />
-          <Statistics text="bad" stat={bad} />
-          <Statistics text="all" stat={all} />
-          <Statistics text="average" stat={average} />
-          <Statistics text="positive" stat={positive} />
-        </tbody>
-      </table>
+      <p>{props.anecdotes[selected]}</p>
+      <Button handleBtn={newQuote} text="next anedocte" />
     </div>
   );
 };
 
-const App = () => {
-  //states
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-
-  return (
-    <div>
-      <h2>give feedback</h2>
-      <Button clickHandle={() => setGood(good + 1)} text="good" />
-      <Button clickHandle={() => setNeutral(neutral + 1)} text="neutral" />
-      <Button clickHandle={() => setBad(bad + 1)} text="bad" />
-      <Stats good={good} neutral={neutral} bad={bad} />
-    </div>
-  );
-};
-
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(<App anecdotes={anecdotes} />, document.getElementById("root"));
