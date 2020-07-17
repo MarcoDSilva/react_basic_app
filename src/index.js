29db5a24a29ca1c6ec21a3abbfde3bc154a4e0eb
MarcoDSilva <38049196+MarcoDSilva@users.noreply.github.com>
2020-07-17 21:57:52 +0100

    phonebook branch

--- src/index.js
+++ src/index.js
@@ -2,28 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from './App'
 
-const notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2020-07-06T19:49:30.098Z",
-    important: true,
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2020-07-06T19:49:30.098Z",
-    important: false,
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2020-07-06T19:49:30.098Z",
-    important: true,
-  },
-];
 
 ReactDOM.render(
-  <App notes={notes} />,
+  <App />,
   document.getElementById('root')
 )
