59f8b7ef954f590f5b87fa1076ceb28550c6f13b
MarcoDSilva <38049196+MarcoDSilva@users.noreply.github.com>
2020-07-31 19:27:45 +0100

    inline style and error msg

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
 import Note from "./components/note";
 import noteService from "./services/notes.js";
+import Notification from "./components/Notification.js";
+import Footer from './components/Footer.js'
 
 const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("");
   const [showAll, setShowAll] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   useEffect(() => {
     noteService.getAll().then((res) => {
@@ -19,7 +22,7 @@ const App = () => {
       content: newNote,
       date: new Date().toISOString(),
       important: Math.random() > 0.5,
-      id: notes.length + 1
+      id: notes.length + 1,
     };
 
     // with post axios, we update the api in our localhost
@@ -35,6 +38,7 @@ const App = () => {
     setNewNote(e.target.value);
   };
 
+  // updating if the note is important or !important
   const toggleImportanceOf = (id) => {
     const note = notes.find((n) => n.id === id);
     const changedNote = { ...note, important: !note.important };
@@ -44,9 +48,16 @@ const App = () => {
       .then((res) => {
         setNotes(notes.map((note) => (note.id !== id ? note : res)));
       })
-      .catch(err => {
-        alert(`the note '${note.content}' was already deleted from the server`)
-        setNotes(notes.filter(n => n.id !== id))
+      .catch((err) => {
+        setErrorMessage(
+          `the note '${note.content}' was already deleted from the server`
+        );
+
+        setTimeout(() => {
+          setErrorMessage(null);
+        }, 5000);
+
+        setNotes(notes.filter((n) => n.id !== id));
       });
   };
 
@@ -55,6 +66,8 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
+
       <button onClick={() => setShowAll(!showAll)}>
         show {showAll ? "important" : "all"}
       </button>
@@ -73,6 +86,8 @@ const App = () => {
         <input value={newNote} onChange={handleNoteChange} />
         <button type="submit">save</button>
       </form>
+
+      <Footer />
     </div>
   );
 };
